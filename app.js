30b9b71937c4de721447e90c4900ca929398d543
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2020-06-21 18:45:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
 extended:true
 }));
-mongoose.connect("mongodb://localhost:27017/wikiDB");
+mongoose.connect("mongodb://localhost:27017/wikiDB",{ useNewUrlParser: true , useUnifiedTopology: true  });
+const articleSchema = {
+    title:String,
+    content:String
+}
+const Article = mongoose.model("Article",articleSchema);
+
 app.use(express.static("public"));
+app.get("/articles",function(req,res){
+   Article.find(function(err,foundArticles){
+       if(!err){
+    res.send(foundArticles);
+       }else{
+           res.send(err);
+       }
+   })
+})
 app.listen(3000,function(){
     console.log("server running on port 3000");
 })
