cb5541ebb58555ed09a238451d8346bb5527641f
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2020-06-22 12:58:26 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,23 +8,24 @@ app.use(bodyParser.urlencoded({
 extended:true
 }));
 mongoose.connect("mongodb://localhost:27017/wikiDB",{ useNewUrlParser: true , useUnifiedTopology: true  });
+app.use(express.static("public"));
 const articleSchema = {
     title:String,
     content:String
 }
 const Article = mongoose.model("Article",articleSchema);
-
-app.use(express.static("public"));
-app.get("/articles",function(req,res){
-   Article.find(function(err,foundArticles){
-       if(!err){
-    res.send(foundArticles);
-       }else{
-           res.send(err);
-       }
-   });
-});
-app.post("/articles",function(req,res){
+//////////////////////////////resquests targeting all articles////////////////////////////////////
+app.route("/articles")
+.get(function(req,res){
+    Article.find(function(err,foundArticles){
+        if(!err){
+     res.send(foundArticles);
+        }else{
+            res.send(err);
+        }
+    });
+ })
+ .post(function(req,res){
     const newArticle = new Article({
         title:req.body.title,
         content:req.body.content
@@ -37,12 +38,23 @@ app.post("/articles",function(req,res){
         }
     });
 })
-app.delete("/articles",function(req,res){
+.delete(function(req,res){
     Article.deleteMany(function(err){
         if(!err){
             res.send("successfully deleted");
         }else{
             res.send(err);
+        }
+    })
+});
+//////////////////////////////resquests targeting specific articles////////////////////////////////////
+app.route("/articles/:articleTitle")
+.get(function(req,res){
+    Article.findOne({title:req.params.articleTitle},function(err,foundArticle){
+        if(foundArticle){
+            res.send(foundArticle);
+        }else{
+            res.send("no articles found");
         }
     })
 })
